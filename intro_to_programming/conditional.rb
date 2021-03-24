a97899d842d837d96a3e0c4e6519218a2b515076
SarahBunker <ChemEnCoder@gmail.com>
2021-03-24 16:56:37 +0000

    working through flow control, still need to do true/false, exercises, and ANKI

---
+++ intro_to_programming/conditional.rb
@@ -0,0 +1,10 @@
+puts "Put in a number"
+a = gets.chomp.to_i
+#chaining methods to get input as a string and convert to an integer
+if a == 3
+  puts "a is 3"
+elsif a == 4
+  puts "a is 4"
+else
+  puts "a is neither a nor 4"
+end
