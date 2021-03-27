9d10b68bc6ac62396f11e22aef49fd78f6a26ca5
SarahBunker <ChemEnCoder@gmail.com>
2021-03-27 19:27:01 +0000

    finished exercises, still need to add ANKI notes for recursion exercise

--- intro_to_programming/loop_iterators_exercises.rb
+++ intro_to_programming/loop_iterators_exercises.rb
@@ -26,7 +26,8 @@ puts ""
 
 loop do 
   puts "type command"
-  user_command = gets.chomp
+  #user_command = gets.chomp
+  user_command = "STOP"
   if user_command != "STOP"
     puts "does stuff"
   end
@@ -36,9 +37,59 @@ loop do
 end
 
 
-user_command = ""
+#user_command = ""
+user_command = "STOP"
 while user_command != "STOP"
   puts "type STOP to end"
   puts "What do you want to do next?"
   user_command = gets.chomp
 end
+
+puts ""
+puts "Problem 3, recursion"
+puts ""
+puts "what number to start with?"
+user_number = gets.chomp.to_i
+x=0
+puts "Let the count down begin"
+def count_down(x,user_number)
+  if x <= user_number
+    count_down(x + 1,user_number)
+    puts x
+  end
+end
+count_down(x,user_number)
+
+puts ""
+puts "Solution"
+#Solution
+def count_to_zero(number)
+  if number <= 0
+    puts number
+  else
+    puts number
+    count_to_zero(number-1)
+  end
+end
+
+count_to_zero(10)
+
+# Their solution printed the number before recursing.
+# mine had the print statement after
+# mine also only had an if statement, not if else because it natually deals with negative numbers.
+#   because when x = 0 it will never be less then a negative number
+# mine is bulkier because it needed two inputs
+
+=begin
+ANKI questions to add later
+Where does the iteration of the counter generally occur in a recursive model?
+in the parenthesis of the call to the method from within the method.
+
+def doubler(start)
+  puts start
+  if start < 10
+    doubler(start * 2)
+  end
+end
+
+=end
